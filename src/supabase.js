991295c39a34ff9007832effbd2ba8a6120792d6
vcachat <valeriecachat@yahoo.com>
2026-04-24 19:10:57 -0400

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://nshglhvnczhvdgfxjetb.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zaGdsaHZuY3podmRnZnhqZXRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5MDMxMjAsImV4cCI6MjA5MjQ3OTEyMH0.We1aq1O1lYI_J8iJVdtd8SGTFjvsiLBeG-5WKkSZ6B0'
+const SUPABASE_ANON_KEY = 'sb_publishable_Em8lwSLTdbYRiKwUkw-jYw_Sj0o_xpY'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
